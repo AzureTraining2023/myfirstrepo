3c4d160182b629407c2f047b82976f31876d2117
anitha ramesh <brr.anitha@gmail.com>
2023-07-03 16:02:59 +0000

    my second commit

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
-class Hello
+public class Hello
 {
 	public string void main(String args[])
 	{
-		System.out.println("This is my first Java Program");
+		System.out.println("This is my first Java Program#######");
 	}
 }

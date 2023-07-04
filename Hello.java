6b23f38b1b05a3edff132a662fc1a127c7765fa3
AzureTraining2023 <133580362+AzureTraining2023@users.noreply.github.com>
2023-07-04 08:49:04 +0530

    Update Hello.java for the second time

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
 	public string void main(String args[])
 	{
-		System.out.println("Hello from Child from remote");
+		System.out.println("Hello from Child from remote for the second time");
 	}
 }
